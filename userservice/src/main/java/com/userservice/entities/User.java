c7906114a3c02adda72f8d75c509a8916ad9a9ff
Prathameshghorapade <pghorapade17@gmail.com>
2026-01-19 14:47:51 +0530

    USer_Service

--- userservice/src/main/java/com/userservice/entities/User.java
+++ userservice/src/main/java/com/userservice/entities/User.java
@@ -1,14 +1,14 @@
 package com.userservice.entities;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -30,6 +30,9 @@ public class User {
 
     @Column(name = "About",length = 50)
     private String about;
+
+    @Transient
+    private List<Rating>ratings=new ArrayList<>();
 
 
 
